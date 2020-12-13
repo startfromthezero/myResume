e13cb77ad36ffaf291e0f52642f5b3715c800e53
startfromthezero <359976215@qq.com>
2020-12-13 20:39:01 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import App from './App.vue'
 import router from "./router/index";
 import store from "./store/index";
 import './index.css'
+import './assets/scss/index.scss'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,3 +1,18 @@
 <template>
-    Create
+    <div>
+        <zst-button theme="primary">primary</zst-button>
+        <zst-button theme="success">success</zst-button>
+        <zst-button theme="danger">danger</zst-button>
+        <zst-button theme="info">info</zst-button>
+        <zst-button theme="warning">warning</zst-button>
+    </div>
 </template>
+<script lang="ts">
+import ZstButton from "../components/Button.vue";
+import { defineComponent } from "vue"
+export default defineComponent({
+    components :{
+        ZstButton
+    }
+})
+</script>
